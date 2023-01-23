5ab2d5276d0cf90dbc84eb48a322521f5b749d88
Kolapo Oni <onikolapo@gmail.com>
2023-01-23 22:11:26 +0100

    implement distortion effect on background image

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <img class="home__background-img" src="" alt="song cover image" />
     <div class="home__overlay">
       <div class="home__song-cover">
-        <img :src="this.song.album.images[0].url" alt="song cover image" />
+        <img :src="song.album.images[0].url" alt="song cover image" />
       </div>
       <p class="home__song-title">{{ song.name }}</p>
       <p class="home__song-artist">{{ song.artists[0].name }}</p>
@@ -22,22 +22,80 @@
 import api from '@/utils/api.js'
 
 export default {
-  data() {
-    return {
-      songTitle: null,
-    }
-  },
   async asyncData() {
     try {
       const { data } = await api.fetchLastPlayedSong()
-      console.log(data.album)
       return {
         song: data,
       }
     } catch (error) {}
   },
-  mounted() {},
-  methods: {},
+  data() {
+    return {}
+  },
+  mounted() {
+    if (process.browser) this.initImageDistortionEffect()
+  },
+  methods: {
+    initImageDistortionEffect() {
+      let app, image
+
+      const setImageScale = () => {
+        image.width = window.innerWidth
+        image.height = window.innerHeight
+        image.position.set(app.screen.width / 2, app.screen.height / 2)
+        image.anchor.set(0.5)
+
+        let scale = 1
+        if (image.texture.height < image.texture.width) {
+          scale = app.screen.height / image.texture.height
+        } else scale = app.screen.width / image.texture.width
+        image.scale.set(scale)
+      }
+
+      const initPixi = () => {
+        app = new PIXI.Application({
+          view: document.querySelector('.home__canvas'),
+          width: window.innerWidth,
+          height: window.innerHeight,
+          transparent: true,
+        })
+
+        /* eslint new-cap: ["error", { "newIsCap": false }] */
+        const coverImageurl = this.song.album.images[0].url
+        image = new PIXI.Sprite.from(coverImageurl)
+
+        PIXI.Loader.shared
+          .add('song cover image', coverImageurl)
+          .load(setImageScale)
+
+        app.stage.addChild(image)
+
+        const displacementSprite = new PIXI.Sprite.from(
+          require('@/assets/images/displacement-image.jpeg')
+        )
+
+        const displacementFilter = new PIXI.filters.DisplacementFilter(
+          displacementSprite
+        )
+
+        displacementFilter.scale.set(60)
+
+        displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT
+
+        app.stage.addChild(displacementSprite)
+
+        app.stage.filters = [displacementFilter]
+
+        app.ticker.add((delta) => {
+          displacementSprite.x += 6
+          displacementSprite.y += 4
+        })
+      }
+
+      initPixi()
+    },
+  },
 }
 </script>
 
